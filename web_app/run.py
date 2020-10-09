c4fa2df04cc38e5043de626e826f4643172e17b9
p.olifer <pavel.olifer@gmail.com>
2020-10-09 20:40:24 +0300

    use BackgroundScheduler instead of AsyncIOScheduler, last didnt work

--- web_app/run.py
+++ web_app/run.py
@@ -7,18 +7,13 @@ __author__ = 'p2lly'
 '''
 
 
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-import asyncio
-from web_app.src.currency import refresh_rates_schd
+from apscheduler.schedulers.background import BackgroundScheduler
+from web_app.src.currency import refresh_rates_scheduled
 from web_app import app
+
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(refresh_rates_scheduled, "interval", hours=24)
+scheduler.start()
+
 app.run(host='0.0.0.0', port=5000, debug=True)
-
-
-if __name__ == '__main__':
-    scheduler = AsyncIOScheduler()
-    scheduler.add_job(refresh_rates_schd, 'interval', hours=24)
-    scheduler.start()
-    try:
-        asyncio.get_event_loop().run_forever()
-    except (KeyboardInterrupt, SystemExit):
-        pass
